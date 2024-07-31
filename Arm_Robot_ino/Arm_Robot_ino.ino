15d4284c2577427c1d0b500fe04150a19ffdcb70
ahmedelsherbiny74-eng <ahmedelsherbiny7474@gmail.com>
2024-08-01 01:47:40 +0300

    Update switching between modes

--- Arm_Robot_ino/Arm_Robot_ino.ino
+++ Arm_Robot_ino/Arm_Robot_ino.ino
@@ -12,18 +12,21 @@ Servo V_SERVO;
 #define JoystickSwitch A2
 #define Buz 6
 #define Led 4
+
+char PreviousMode = ' ';
+char CurrentMode = 'M';
+char FireButton;
+bool On = false;
+int FireRate;
 int angelX;
-int angelY; 
+int angelY;
 
 void CheckFlame();
 void AutomaticMotion();
 void JoystickManualMotion();
 void ManualMotion();
 void CheckMode();
-bool On = false;
-char PreviousMode; 
-char CurrentMode;
-char FireButton;
+
 void setup() {
   pinMode(Buz, OUTPUT);
   pinMode(Led, OUTPUT);
@@ -38,165 +41,155 @@ void setup() {
   delay(400);
   Serial.begin(9600);
   BTSerial.begin(9600);
-  
 }
 
 void loop() {
   CheckMode();
 }
 
-void CheckMode(){
-if(BTSerial.available()) {
+void CheckMode() {
+  if (BTSerial.available()) {
     CurrentMode = BTSerial.read();
-    // Serial.print("Received character: ");
-    // Serial.println(CurrentMode);
-    if(CurrentMode == 'A') {
-      On = false;
-      PreviousMode='A';
-      On=true;
-      AutomaticMotion();
-      delay(50);
-    }
-    else if(CurrentMode == 'M') {
-      On=false;
-      PreviousMode='M';
-      On=true;
-      ManualMotion();
-      delay(50);  
-    }
-    else if(CurrentMode == 'J') {
-      On=false;
-      PreviousMode='J';
-      On = true;
-      JoystickManualMotion();
-      delay(50);  
+  }
+  
+  if (CurrentMode != PreviousMode) {
+    On = false;  // Stop the current mode
+    delay(100);  // Short delay to ensure the current mode stops
+    Serial.flush();
+    BTSerial.flush();
+
+    PreviousMode = CurrentMode;  // Update previous mode
+
+    switch (CurrentMode) {
+      case 'A':
+        On = true;
+        AutomaticMotion();
+        break;
+      case 'M':
+        On = true;
+        ManualMotion();
+        break;
+      case 'J':
+        On = true;
+        JoystickManualMotion();
+        break;
     }
   }
   delay(50);
-  
 }
 
 void CheckFlame() {
-  Serial.println(map(analogRead(Flame), 0, 1023, 255, 0));
+  // Serial.println(map(analogRead(Flame), 0, 1023, 255, 0));
   if (analogRead(Flame) < 450) {
     digitalWrite(Led, HIGH);
-    while(true) {
-      // We Will Creat A Button In the App To Break This While Loop And Make The Arm Continue The Motion
-      analogWrite(Buz, map(analogRead(Flame), 0, 1023, 255, 0));
-      // Serial.println(analogRead(Flame));
-      if(BTSerial.available()) 
-      FireButton = BTSerial.read();
-      if ((digitalRead(JoystickSwitch) == LOW && PreviousMode=='J' ) || (FireButton=='S' && PreviousMode=='A' ) || (FireButton=='S' && PreviousMode=='M' ))  {           // To Continue The Motion From Joystick Switch   
+    while (true) {
+      FireRate = map(analogRead(Flame), 0, 1023, 255, 0);
+      analogWrite(Buz, FireRate);
+      // BTSerial.println(FireRate);
+      if (BTSerial.available())
+        FireButton = BTSerial.read();
+      if ((digitalRead(JoystickSwitch) == LOW && CurrentMode == 'J') || 
+          (FireButton == 'S' && (CurrentMode == 'A' || CurrentMode == 'M'))) {
         FireButton = 'Z';
         break;
       }
     }
-  }
-  else
+  } 
+  else {
     digitalWrite(Led, LOW);
     analogWrite(Buz, LOW);
+  }
 }
 
 void AutomaticMotion() {
-  while(On) {
-    for (int i = 5; i <= 175 ; i += 10) {
-    H_SERVO.write(i);
-    delay(30);
-    for (int i = 10; i <= 60 ; i++) {
-      V_SERVO.write(i);
-      CheckFlame();
-      CheckMode();
+  while (On) {
+    delay(100);
+    for (int i = 5; i <= 175; i += 10) {
+      H_SERVO.write(i);
+      Serial.println(i);
       delay(30);
-      if(!On)
-      return 0;
-      // Serial.println(i);
+      for (int j = 10; j <= 60; j++) {
+        V_SERVO.write(j);
+        CheckFlame();   
+        Serial.println(j);
+        CheckMode();
+        delay(30);
+      }
+      delay(50);  
+      for (int j = 60; j >= 10; j--) {
+        V_SERVO.write(j);
+        CheckFlame();
+        Serial.println(j);
+        CheckMode();
+        delay(30);
+      }
     }
-    delay(50);  
-    for (int i = 60; i >= 10; i--) {
-      V_SERVO.write(i);
-      CheckFlame();
-      CheckMode();
+    delay(1000);
+    for (int i = 175; i >= 5; i -= 10) {
+      H_SERVO.write(i);
+      Serial.println(i);
       delay(30);
-      if(!On)
-      return 0;
-      // Serial.println(i);
+      for (int j = 10; j <= 60; j++) {
+        V_SERVO.write(j);
+        CheckFlame();
+        CheckMode();
+        Serial.println(j);
+        delay(30);
+      }
+      delay(50);  
+      for (int j = 60; j >= 10; j--) {
+        V_SERVO.write(j);
+        CheckFlame();
+        CheckMode();
+        Serial.println(j);
+        delay(30);
+      }
     }
-  }
-  delay(1000);
-  for (int i = 175; i >= 5; i-= 10) {
-    H_SERVO.write(i);
-    delay(30);
-    for (int i = 10; i <= 60; i++) {
-      V_SERVO.write(i);
-      CheckFlame();
-      CheckMode();
-      delay(30);
-      if(!On)
-      return 0;
-    }
-    delay(50);  
-    for (int i = 60; i >= 10; i--) {
-      V_SERVO.write(i);
-      CheckFlame();
-      CheckMode();
-      delay(30);
-      if(!On)
-      return 0;
-    }
-  }
-  delay(4000);
+    delay(4000);
   }
 }
 
 void JoystickManualMotion() {
-  while(On) {
+  while (On && CurrentMode == 'J') {
     angelX = map(analogRead(JoystickX), 0, 1023, 170, 10);
     delay(25);
     angelY = map(analogRead(JoystickY), 0, 1023, 60, 10);
     delay(25);
-    if (angelX <= 170 && angelX >= 10)
-      H_SERVO.write(angelX);
+    if (angelX <= 170 && angelX >= 10) H_SERVO.write(angelX);
     delay(25);
-    if (angelY <= 60 && angelY >= 10)
-      V_SERVO.write(angelY);
+    if (angelY <= 60 && angelY >= 10) V_SERVO.write(angelY);
     delay(25);
     CheckFlame();
     delay(30);
     CheckMode();
-    delay(25);
-    // Serial.println(angelX);
-    // Serial.println(angelY);
-    delay(25);
+    delay(30);
   }
 }
 
 void ManualMotion() {
-  while (On) {
+  while (On && CurrentMode == 'M') {
     if (BTSerial.available()) {
-      // Read the incoming string from Bluetooth
-      String s = BTSerial.readStringUntil('\n'); // Assuming commands are newline-terminated
-      if(s.length()==1){
-        CurrentMode=s.charAt(0);
+      String s = BTSerial.readStringUntil('\n');
+      if (s.length() == 1) {
+        CurrentMode = s.charAt(0);
+        Serial.println(CurrentMode);
         break;
       }
-      
-      String letter = s.substring(0, 1); // Assuming the letter is at the start
-      String numbers = s.substring(1);    // Assuming the numbers start from the second character
-      
-      int num = numbers.toInt(); // Convert the numeric part to an integer
-      CheckFlame();
+      String letter = s.substring(0, 1);
+      String numbers = s.substring(1);
+      int num = numbers.toInt();
       if (letter.equals("H")) {
         Serial.println(num);
         H_SERVO.write(num);
         delay(30);
-      } 
-      else if (letter.equals("V")) {
+      } else if (letter.equals("V")) {
         Serial.println(num);
         V_SERVO.write(num);
         delay(30);
       }
     }
+    CheckFlame();
     delay(30);
   }
-   CheckMode(); 
+  CheckMode();
 }
